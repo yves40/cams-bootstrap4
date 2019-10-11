9c0cef8b5f5ba4ba082131c4d611a5b119ce1287
yves40 <yves.toubhans@free.fr>
2019-10-11 10:03:44 +0200

    Add text to test scrolling

--- src/modules/bootstrap4/views/Template.vue
+++ src/modules/bootstrap4/views/Template.vue
@@ -2,7 +2,8 @@
   Template.vue
 
   Oct 04 2019   Initial
-  Oct 05 2019   Initial
+  Oct 05 2019   Add text
+  Oct 11 2019   More text to scroll
 -->
 <template>
   <div class="bootstrap" id="template">
@@ -41,6 +42,16 @@
           <b-row>
             <b-col cols="2"></b-col>
             <b-col>
+              <div>
+                <p>Here comes the application zone</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum rem dignissimos illum distinctio in, nesciunt, doloribus, ab optio et numquam dolore praesentium perferendis placeat necessitatibus. Harum, voluptates odit ipsam quas aut soluta, a amet natus vitae tenetur sapiente. Magni nulla culpa animi. Mollitia placeat, corporis aperiam soluta saepe porro delectus harum voluptates rerum? Id nihil modi ipsa sunt velit obcaecati ex illum nemo nesciunt eos rem consequatur nobis quae aperiam facilis tempore laudantium saepe minima, amet dicta soluta dolores? Voluptas officiis quod, molestias veniam eum quasi consequuntur voluptatum, dicta sequi modi in sit blanditiis nostrum similique, doloribus amet explicabo a!</p>
+                <p>Do whatever you want into this zone</p>
+              </div>              
+              <div>
+                <p>Here comes the application zone</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum rem dignissimos illum distinctio in, nesciunt, doloribus, ab optio et numquam dolore praesentium perferendis placeat necessitatibus. Harum, voluptates odit ipsam quas aut soluta, a amet natus vitae tenetur sapiente. Magni nulla culpa animi. Mollitia placeat, corporis aperiam soluta saepe porro delectus harum voluptates rerum? Id nihil modi ipsa sunt velit obcaecati ex illum nemo nesciunt eos rem consequatur nobis quae aperiam facilis tempore laudantium saepe minima, amet dicta soluta dolores? Voluptas officiis quod, molestias veniam eum quasi consequuntur voluptatum, dicta sequi modi in sit blanditiis nostrum similique, doloribus amet explicabo a!</p>
+                <p>Do whatever you want into this zone</p>
+              </div>              
               <div>
                 <p>Here comes the application zone</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum rem dignissimos illum distinctio in, nesciunt, doloribus, ab optio et numquam dolore praesentium perferendis placeat necessitatibus. Harum, voluptates odit ipsam quas aut soluta, a amet natus vitae tenetur sapiente. Magni nulla culpa animi. Mollitia placeat, corporis aperiam soluta saepe porro delectus harum voluptates rerum? Id nihil modi ipsa sunt velit obcaecati ex illum nemo nesciunt eos rem consequatur nobis quae aperiam facilis tempore laudantium saepe minima, amet dicta soluta dolores? Voluptas officiis quod, molestias veniam eum quasi consequuntur voluptatum, dicta sequi modi in sit blanditiis nostrum similique, doloribus amet explicabo a!</p>
