96f81d2c573a305353cf09efafbabc805e0a55e2
yves40 <yves.toubhans@free.fr>
2019-10-23 20:03:32 +0200

    Add mongostore

--- src/modules/core/store/store.js
+++ src/modules/core/store/store.js
@@ -4,6 +4,7 @@
     Oct 06 2019     Initial
     Oct 05 2019     Reorg
     Oct 06 2019     Install common services
+    Oct 23 2019     Add mongo 
 ----------------------------------------------------------------------------*/
 
 import Vue from 'vue';
@@ -14,10 +15,12 @@ const logger = require('../services/logger');
 // Get various stores
 import corestore from './corestore';
 import userstore from '../../users/store/userstore';
+import mongostore from './mongostore';
 
 export default new Vuex.Store({
     modules: {
         corestore: corestore ,
         userstore: userstore,
+        mongostore: mongostore,
     }
   });
