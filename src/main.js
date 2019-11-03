5cca603deb9b983bff238ffa5d8ffa69b318ea53
yves40 <yves.toubhans@free.fr>
2019-11-03 15:13:51 +0100

    vue-swal

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@
 //    Oct 05 2019   Reorg routers
 //    Oct 06 2019   1st vuex tests
 //    Oct 16 2019   Reorg folders : router becomes vuerouter
+//    Nov 03 2019   vueswal for informational popups
 //----------------------------------------------------------------------------
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './css/main.css'
 import BootstrapVue from 'bootstrap-vue'
+import vueswal from 'vue-swal'
 
 import Vue from 'vue';
 import App from './App';
@@ -17,9 +19,10 @@ import App from './App';
 import mainrouter from './modules/core/vuerouter/mainrouter';
 import store from './modules/core/store/store';
 
-const Version = 'Version:1.06, Oct 16 2019';
+const Version = 'Version:1.08, Nov 03 2019';
 
 Vue.use(BootstrapVue);
+Vue.use(vueswal);   // For smart alert popups
 
 new Vue({
   el: '#app',
