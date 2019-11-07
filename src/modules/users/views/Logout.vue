7da4d723742d6516d79a5576f8bf6de8773e907e
yves40 <yves.toubhans@free.fr>
2019-11-07 10:22:41 +0100

    Added for some dynamic menu purpose...

---
+++ src/modules/users/views/Logout.vue
@@ -0,0 +1,63 @@
+<!--
+
+  Logout.vue
+  
+  Jan 31 2019   Initial
+  Feb 06 2019   Simplify axios   
+  Feb 08 2019   axiosutility...
+  Mar 15 2019   Test token invalidation after logout. 
+                No longer delete it from local storage
+                Instead, shorten its expiration time
+  Nov 07 2019   cams-Bootstrap4
+ 
+-->
+<template>
+
+</template>
+
+<script>
+
+import { mapActions } from 'vuex'
+
+export default {
+  data: () => ({
+    Version: 'Logout:1.19, Nov 07 2019 ',
+  }),
+  mounted() {
+    this.logout();
+  },
+  methods: {
+    ...mapActions(
+      'userstore', {
+        logoutVuex: 'logout',
+      }
+    ),
+    // --------------------------------- Logging out  --------------------------------
+    logout() {
+      this.logoutVuex({router: this.$router, path: this.$route.path})
+        .then((result) => {
+          this.$swal('You are disconnected!', result, 'success');
+          this.$parent.disableMenu('logout');
+          this.$parent.enableMenu('login');
+          this.$parent.enableMenu('register');
+        })
+        .catch((err) => {
+          this.$swal('KO!', err, 'error');
+        });
+    },
+  },
+};
+</script>
+
+<style>
+  h1 {
+    text-align: left;
+    font-family: 'Courier New', Courier, monospace;
+    font-size: 2em;
+  }
+  h2 {
+    text-align: left;
+    color: blue;
+    font-size:1em;
+  }
+</style>
