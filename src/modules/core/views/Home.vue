7b29efc80f4c3aa1d300dbd02c3398a99e2cbc56
yves40 <yves.toubhans@free.fr>
2019-11-07 17:01:59 +0100

    Updated comments...

--- src/modules/core/views/Home.vue
+++ src/modules/core/views/Home.vue
@@ -9,6 +9,7 @@
   Oct 05 2019   Reorg
   Oct 06 2019   1st vuex tests
   Oct 09 2019   Small change in content
+  Nov 07 2019   Small change in content
 -->
 <template>
   <div>
@@ -34,16 +35,26 @@
                 This is the temporary home page. This application will manage webcams.
                 The goal is to easily monitor their status, set alarm on and off, based on time schedule,
                 and then get the Video stream and display it in one or more windows.
-                Some of the used technologies are :
+                This is an MEVN based application. MEVN stands for Mongodb, Express, Vuejs, Nodejs.
+                The application uses technologies such as :
               </p>
-                <b-list-group >
-                  <b-list-group-item >MongoDB</b-list-group-item>
-                  <b-list-group-item >Vue</b-list-group-item>
-                  <b-list-group-item >Vuex</b-list-group-item>
-                  <b-list-group-item >Bootstrap and Bootstrap-Vue</b-list-group-item>
-                  <b-list-group-item >Nodejs</b-list-group-item>
-                  <b-list-group-item >Express</b-list-group-item>
-                </b-list-group>
+              <b-list-group >
+                <b-list-group-item >MongoDB, mongoose</b-list-group-item>
+                <b-list-group-item >Express</b-list-group-item>
+                <b-list-group-item >Vue, Vuex</b-list-group-item>
+                <b-list-group-item >Nodejs</b-list-group-item>
+                <b-list-group-item >Bootstrap and Bootstrap-Vue</b-list-group-item>
+                <b-list-group-item >Axios, passport, json web token</b-list-group-item>
+              </b-list-group>
+              <p></p>
+              <p>
+                And some other libraries...
+              </p>
+              <p>
+                The goal is to learn some modern dev techniques.
+                Seems pretty simple for young development experts, but for an old timer like me, who worked on SQL, 
+                PL/SQL, C, C++, Java, Struts, Servlets, etc. during part of its life, its a huge challenge ;-)
+              </p>
           </b-col>
           <b-col cols="2"></b-col>
         </b-row>
