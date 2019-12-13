b7e731e41391cbabfbf9f4fc3b825abdbb17381a
yves40 <yves.toubhans@free.fr>
2019-12-13 11:40:07 +0100

    WIP on log filters

--- src/modules/users/views/Identity.vue
+++ src/modules/users/views/Identity.vue
@@ -5,6 +5,10 @@
   Nov 05 2019   Initial
   Nov 29 2019   Manage user info
   Dec 09 2019   Get some user login logout...logs
+                Event date is formatted with a discovered Vue feature called Vue.filter
+                Follow the formatdate track...
+  Dec 12 2019   Add some info about user activity
+  Dec 13 2019   Filter on user activity logs
 -->
 <template>
   <div>
@@ -25,6 +29,9 @@
         <b-col cols="2"></b-col>
       </b-row>
 
+      <!-- 
+        User data
+      -->
       <div class="viewframe">
         <b-row>
           <b-col cols="2"></b-col>
@@ -70,22 +77,60 @@
           <b-col cols="2"></b-col>
         </b-row>
       </div>
+      <!-- 
+        Logs
+      -->
       <div class="viewframe">
 
-        <b-row>
+        <!-- 
+          The log dump header
+        -->
+        <b-row class="logheader">
           <b-col cols="2"></b-col>
-          <b-col>
+          <b-col >
             <p>User recent activity</p>
           </b-col>
           <b-col cols="2"></b-col>
         </b-row>
-        <div class="viewframe" v-for="entry in userlogs" :key="entry.id">
+        <!-- 
+          Filter with check boxes
+        -->
+        <b-row >
           <b-col cols="2"></b-col>
           <b-col >
+                <b-form-checkbox-group v-model="filterbox" id="checkboxes">
+                  <b-form-checkbox value="deb" unchecked-value="nodeb">Debug</b-form-checkbox>
+                  <b-form-checkbox value="inf" unchecked-value="noinf">Information</b-form-checkbox>
+                  <b-form-checkbox value="war">Warning</b-form-checkbox>
+                  <b-form-checkbox value="err">Error</b-form-checkbox>
+                  <b-form-checkbox value="fat">Fatal</b-form-checkbox>
+                </b-form-checkbox-group>
+          </b-col>
+          <b-col cols="2"></b-col>
+        </b-row>
+        <!-- 
+          The log dump window 
+        -->
+        <div class="viewframe" v-for="entry in userlogs" :key="entry.id">
+          <b-col cols="2"></b-col>
+          <b-col v-if="entry.severity < '2'" class="loginf">
+            {{entry.timestamp | formatdate}} - {{entry.message}}
+          </b-col>
+          <b-col v-else-if="entry.severity === '2'" class="logwarn">
+            {{entry.timestamp | formatdate}} - {{entry.message}}
+          </b-col>
+          <b-col v-else-if="entry.severity === '3'" class="logerr">
+            {{entry.timestamp | formatdate}} - {{entry.message}}
+          </b-col>
+          <b-col v-else-if="entry.severity === '4'" class="logfatal">
+            {{entry.timestamp | formatdate}} - {{entry.message}}
+          </b-col>
+          <b-col v-else class="logfatal">
             {{entry.timestamp | formatdate}} - {{entry.message}}
           </b-col>
           <b-col cols="2"></b-col>
         </div>
+
       </div>
     </b-container>
   </div>
@@ -103,7 +148,8 @@ import { mapGetters } from 'vuex'
 export default {
   data() {
       return {
-        version: "Identity 1.08, Dec 09 2019 ",
+        version: "Identity 1.37, Dec 13 2019 ",
+        filterbox: [ 'deb', 'inf', 'war', 'err', 'fat'],
       };
   },
   // ------------------------------------------------------------------------------------------------------------
@@ -120,6 +166,7 @@ export default {
         },
     ),
   },  
+  
   // ------------------------------------------------------------------------------------------------------------
   created() {
     this.$parent.disableMenu('identity');
@@ -132,6 +179,13 @@ export default {
     gotohome() {
       this.$router.push({ name: 'home' });
     },
+  },
+  watch: {
+    filterbox: {
+      handler: (value) => {
+        console.log('Filters  set to : ' + value);
+      }
+    },
   }
-};
+}
 </script>
