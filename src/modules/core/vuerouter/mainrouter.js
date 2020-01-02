ff34d40d2d067b9c71af6ec9d62251ed19dd6761
yves40 <yves.toubhans@free.fr>
2020-01-02 18:43:48 +0100

    notyet page

--- src/modules/core/vuerouter/mainrouter.js
+++ src/modules/core/vuerouter/mainrouter.js
@@ -4,15 +4,17 @@
 //    Oct 05 2019   Initial : core router 
 //    Oct 09 2019   About
 //    Oct 16 2019   Reorg folders : router becomes vuerouter
+//    Jan 02 2020   Not yet page
 //----------------------------------------------------------------------------
 
-const Version = "mainrouter.js: Oct 16 2019, 1.21 ";
+const Version = "mainrouter.js: Jan 02 2020, 1.22 ";
 
 import Vue from "vue";
 import Router from "vue-router";
 
 import Home from "../views/Home";
 import About from "../views/About";
+import Notyet from "../views/Notyet";
 // Additional routes for the template
 import bootstrap4routes from '../../bootstrap4/vuerouter/bootstrap4router';
 import usersroutes from '../../users/vuerouter/usersrouter';
@@ -23,6 +25,7 @@ const  mainroutes =
   [
     { path: "/", name: "/", component: Home },
     { path: "/home", name: "home", component: Home },
+    { path: "/notyet", name: "notyet", component: Notyet },
     { path: "/about", name: "about", component: About },
   ];
 
