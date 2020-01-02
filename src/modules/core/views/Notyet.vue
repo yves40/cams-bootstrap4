cb180a62db1a9fce78897b31d8f5184b870ba924
yves40 <yves.toubhans@free.fr>
2020-01-02 18:43:27 +0100

    To manage missing functionalities

---
+++ src/modules/core/views/Notyet.vue
@@ -0,0 +1,54 @@
+<!--
+
+  Notyet.vue
+
+  Jan 02 2020   Initial
+-->
+<template>
+  <div>
+      <b-container>
+        <b-row>
+          <b-col cols="2"></b-col>
+          <b-col>
+            <img src="../../../assets/WIP.png" />
+          </b-col>
+          <b-col cols="2"></b-col>
+        </b-row>
+        <b-row>
+          <b-col cols="2"></b-col>
+          <b-col>
+            <b-list-group >
+              <b-list-group-item>{{version}}</b-list-group-item>
+            </b-list-group>
+          </b-col>
+          <b-col cols="2"></b-col>
+        </b-row>
+        <b-row>
+          <b-col cols="2"></b-col>
+          <b-col>
+            <p></p> 
+            <p> This feature is not yet implemented.</p>
+            <p></p> 
+            <p>We're working hard to bring it online asap</p> 
+          </b-col>
+          <b-col cols="2"></b-col>
+        </b-row>
+      </b-container>
+  </div>
+</template>
+
+<script>
+
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+
+  },
+  data() {
+    return {
+      version: "Notyet 1.00, Jan 02 2020"
+    };
+  },
+};
+</script>
