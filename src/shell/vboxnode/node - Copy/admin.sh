1c5b4e1c3143244ee5ed4d574183b608bfb58329
yves40 <yves.toubhans@free.fr>
2020-02-29 19:03:59 +0100

    Sync

---
+++ src/shell/vboxnode/node - Copy/admin.sh
@@ -0,0 +1,193 @@
+#--------------------------------------------------------------------------------
+#	admin.sh
+#
+#	Feb 29 2020  	Initial
+#--------------------------------------------------------------------------------
+VERSION="admin.sh v 1.04, "
+VERSIONDATE="Feb 29 2020 "
+LOG="/tmp/nodeadmin.log"
+CAMSHOME='/home/node/cams'
+#--------------------------------------------------------------------------------
+# Logger
+#--------------------------------------------------------------------------------
+log()
+{
+        echo "`date '+%Y-%m-%d %H:%M:%S'` : $VERSION $1" >> $LOG
+        echo "`date '+%Y-%m-%d %H:%M:%S'` : $1"
+}
+#---------------------------------------------------------------------------------------
+#   usage
+#---------------------------------------------------------------------------------------
+Usage()
+{
+  echo
+  echo
+  echo "./admin.sh start|stop|status [procselector]"
+  echo 
+  echo "With start, can optionnaly specify a procselector."
+  echo "Possible values are : all|web|api|mongo"
+  echo
+  echo
+}
+#---------------------------------------------------------------------------------------
+#   Node processes startup
+#---------------------------------------------------------------------------------------
+Start()
+{
+  curdir=`pwd`
+  cd $CAMS
+  echo
+  echo
+
+  case $proclist in 
+    ALL)  log "Start all processes"
+            log "#1 Web app"
+            ret=`pwd`
+            cd $CAMSHOME
+            npm run devx
+            cd $ret
+            log "#2 the API server"
+            pm2 start $CAMSHOME/src/server.js --watch
+            ;;
+    WEB)  log "Start WEB process"
+            log "#1 Web app"
+            ret=`pwd`
+            cd $CAMSHOME
+            ls -l
+            npm run devx
+            cd $ret
+            cat nohup.out
+            ;;
+    API)  log "Start API processes"
+            log "#2 the API server"
+            pm2 start $CAMSHOME/src/server.js --watch
+            ;;
+    MONGO)  log "Start mongodb"
+            log "#3 the DB server: log in mongo account"
+            sudo systemctl start mongod
+            ;;
+  esac
+
+  echo
+  cd $curdir
+  tput sgr0
+  echo
+}
+#---------------------------------------------------------------------------------------
+#   Node and mongodb processes: stop
+#---------------------------------------------------------------------------------------
+Stop()
+{
+  echo
+  echo
+  echo
+
+  case $proclist in 
+    ALL)    log "Stop all node processes"
+            ps -edf | grep -v grep | grep -i -e 'webpack-dev-server
+nodemon
+/TOOLS/node/bin/node' > processlist
+            while read line
+            do  
+              pid=`echo "$line" | awk '/ / { print $2 }';`
+              log "[!!!] $pid killed"
+              kill $pid
+            done < processlist
+            rm -f processlist
+            ;;
+    WEB)    log "Stop the WEBpack node process"
+            ps -edf | grep -v grep | grep -i -e 'webpack-dev-server' > processlist
+            while read line
+            do  
+              pid=`echo "$line" | awk '/ / { print $2 }';`
+              log "[!!!] $pid killed"
+              kill $pid
+            done < processlist
+            rm -f processlist
+            ;;
+    API)    log "Stop the API node process"
+            pm2 delete all
+            ;;
+    MONGO)  log "Stop mongodb"
+            sudo systemctl stop mongod
+            ;;
+  esac
+  echo
+  echo
+}
+
+#---------------------------------------------------------------------------------------
+#   Node and mongodb processes status
+#---------------------------------------------------------------------------------------
+Status()
+{
+  echo
+  echo 'Node and mongodb processes status'
+  echo
+  ps -edf | grep -v grep | grep -i -e 'webpack-dev-server
+nodemon
+/TOOLS/node/bin/node
+mongod' > processlist
+
+  while read line
+  do  
+    pid=`echo "$line" | awk '/ / { print $2 }';`
+    ppid=`echo "$line" | awk '/ / { print $3 }';`
+    processname=`echo $line | cut -d ' ' -f 8-`
+    log "[] $pid $ppid $processname"
+  done < processlist
+  rm -f processlist
+  echo
+  echo
+}
+
+#---------------------------------------------------------------------------------------
+#   Start here
+#---------------------------------------------------------------------------------------
+clear
+echo
+echo $VERSION $VERSIONDATE
+echo
+if [ -z $1 ]
+then
+  Usage
+  echo
+  exit 1
+fi
+if [ -z $2 ]
+then
+  proclist="ALL"
+else
+  procselector=`echo $2 | tr a-z A-Z`
+  case $procselector in 
+    'ALL')  
+            proclist="ALL"
+            ;;
+    'WEB')  
+            proclist="WEB"
+            ;;
+    'API')  
+            proclist="API"
+            ;;
+    'MONGO')  
+            proclist="MONGO"
+            ;;
+    *)      log "Invalid selector : $procselector"
+            Usage
+            exit 1
+            ;;
+  esac
+fi
+
+case $1 in 
+  'start')  Start
+            ;;
+  'stop')   Stop
+            ;;
+  'status') Status
+            ;;
+  *)        Status
+            ;;
+esac
+
+exit 0
